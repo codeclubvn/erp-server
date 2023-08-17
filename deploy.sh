bc856740b502d5053b82d63751fe882de80d6094
Dương Hiếu <duonghieu0712z@gmail.com>
2023-08-17 15:27:34 +0700

    Update deploy files

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Build
 docker compose up -d --force-recreate --build
 
+# Remove dangling images
 none_images=$(docker images -f "dangling=true" -q)
 num_of_none_images=$(echo "$none_images" | wc -w)
 echo "Docker has '$num_of_none_images' dangling image(s)"
@@ -9,4 +11,14 @@ echo "Docker has '$num_of_none_images' dangling image(s)"
 if [ "$num_of_none_images" -gt 0 ]; then
     docker rmi "$none_images"
     echo "Remove '$num_of_none_images' dangling image(s) in Docker successfully"
+fi
+
+# Remove dangling volumes
+none_volumes=$(docker volume ls -f "dangling=true" -q)
+num_of_none_volumes=$(echo "$none_volumes" | wc -w)
+echo "Docker has '$num_of_none_volumes' dangling volume(s)"
+
+if [ "$num_of_none_volumes" -gt 0 ]; then
+    docker volume rm "$none_volumes"
+    echo "Remove '$num_of_none_volumes' dangling volume(s) in Docker successfully"
 fi
