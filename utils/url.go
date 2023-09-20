07d3608853666a630fbc05b9f88c0c6d2da400cb
hieunguyenduc99 <hieu.nguyen.duc@sobanhang.com>
2023-09-20 21:41:19 +0700

    add func get id

--- utils/url.go
+++ utils/url.go
@@ -1,1 +1,23 @@
 package utils
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+type UriParse struct {
+	ID []string `json:"id" uri:"id"`
+}
+
+func ParseStringIDFromUri(c *gin.Context) string {
+	tID := UriParse{}
+	if err := c.ShouldBindUri(&tID); err != nil {
+		_ = c.Error(err)
+		return ""
+	}
+	if len(tID.ID) == 0 {
+		_ = c.Error(fmt.Errorf("error: Empty when parse ID from URI"))
+		return ""
+	}
+	return tID.ID[0]
+}
